44a2b0c887bfff9bb478cd9b386d15551320ef92
Aniruddha Landge <100419083+AniruddhLandge@users.noreply.github.com>
2025-07-23 10:43:54 +0530

    Update decimalconversion.cpp

--- decimalconversion.cpp
+++ decimalconversion.cpp
@@ -1,39 +1,21 @@
-//decimal to binary
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int num=50;
-//     int mod;
-//     int i=0;
-//     int a[10];
+decimal to binary
+#include<iostream>
+using namespace std;
+int main(){
+    int num=50;
+    int mod;
+    int i=0;
+    int a[10];
 
-//     while(num){
-//     mod=num%2;
-//     num=num/2;
-//     a[i]=mod;
-//     i++;
-//     }
-//     for (int j = i-1; j >=0 ; j--)
-//     {
-//         cout<<a[j];
-//     }
+    while(num){
+    mod=num%2;
+    num=num/2;
+    a[i]=mod;
+    i++;
+    }
+    for (int j = i-1; j >=0 ; j--)
+    {
+        cout<<a[j];
+    }
     
-// }
-
-//binary to decimal
-// #include<iostream>
-// #include<cmath>
-// using namespace std;
-// int main(){
-//     int num=110010;
-//     int ans = 0;
-//     int i=0;
-//     while(num){
-//         int mod = num%10;
-//         num = num/10;
-//         ans = ans + mod*pow(2,i);
-//         i++;
-//     }
-//     cout<<ans;
-//     return 0;
-// }
+}
