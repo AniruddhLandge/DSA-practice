d83b32da99acce9d14b76b7625b06272b580d6ce
Aniruddha Landge <100419083+AniruddhLandge@users.noreply.github.com>
2025-07-23 11:13:53 +0530

    Add Pattern_3.cpp

---
+++ Pattern_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    char ch='A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+}
+
+// A 
+// B C 
+// D E F 
+// G H I J 
+// K L M N O
